baf1b27153923c407e47341251370de13812e259
Doni Wahyu Ramadhan <doniwramadhan@gmail.com>
2024-05-20 15:56:05 +0700

    first

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Doni on 5/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
